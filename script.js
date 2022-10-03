38670452b5d7bb2311fd1c95ef0055ea2e21523d
Mesquita-Dev <lmesquita2k@gmail.com>
2022-10-03 17:28:15 -0300

    Menu Hamburguer

--- script.js
+++ script.js
@@ -22,3 +22,23 @@ function getScrollTopByHref(element) {
   const id = element.getAttribute('href');
   return document.querySelector(id).offsetTop;
 }
+
+//Menu Hamburguer
+const btnMobile = document.getElementById ('btn-mobile');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.querySelector('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  }
+  else{
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
